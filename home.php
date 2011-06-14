b5c34a70ccdd1dbf8b47a343ebe0e955284ffdee
Owen Winkler <a_gihub@midnightcircus.com>
2011-06-14 08:14:39 -0400

    Footer area, header meta stuff, messing around with the loop.

--- home.php
+++ home.php
@@ -1,14 +1,17 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-	<title><?php echo Options::get('title'); ?></title>
+	<title><?php if($request->display_entry && isset($post)) { echo $post->title_title . ' - '; } ?><?php echo Options::get('title'); ?></title>
+	<meta charset="UTF-8">
+	<meta name="generator" content="Habari">
 	<?php
 		Stack::add('template_stylesheet', array('http://yui.yahooapis.com/2.8.1/build/reset-fonts-grids/reset-fonts-grids.css', 'screen,projection'), 'reset');
 		Stack::add('template_stylesheet', array(Site::get_url('theme', '/style.css'), 'screen,projection'), 'site');
 	?>
+	<link rel="Shortcut Icon" href="<?php Site::out_url( 'theme' ); ?>/favicon.png">
 	<?php $theme->header(); ?>
 </head>
-<body id="body">
+<body class="<?php $theme->body_class(); ?>">
 <div id="yui-t1" class="yui-t1 doc-center">
 	<header id="hd">
 		<hgroup>
@@ -24,7 +27,14 @@
 
 		<div id="yui-main">
 			<div id="primary" class="yui-b">
-				<?php foreach($posts as $post) $theme->content($post); ?>
+				<?php if(isset($posts)): ?>
+					<?php foreach($posts as $post) $theme->content($post); ?>
+					<div id="pagenav">
+						<?php $theme->prev_page_link( _t( '&laquo;&nbsp;Newer&nbsp;Posts' ) ); ?><?php $theme->page_selector( null, array( 'leftSide' => 2, 'rightSide' => 2, 'hideIfSinglePage' => true ) ); ?> <?php $theme->next_page_link( _t( 'Older&nbsp;Posts&nbsp;&raquo;' ) ); ?>
+					</div>
+				<?php else: ?>
+					<?php $theme->content($post); ?>
+				<?php endif; ?>
 		  </div>
 		</div>
 		<section id="sidebar" class="yui-b">
@@ -32,7 +42,7 @@
 	  </section>
 	</div>
 			
-  <footer id="ft">My footer stuff</footer>
+  <footer id="ft"><?php $theme->area('footer'); ?></footer>
 </div>
 </body>
 </html>
