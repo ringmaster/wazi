bbed96826dad720c6993cada04495750839edf0f
ringmaster <a_github@midnightcircus.com>
2011-11-24 12:51:03 -0500

    Update with semantic.gs grid framework.

--- home.php
+++ home.php
@@ -4,19 +4,18 @@
 	<title><?php if($request->display_entry && isset($post)) { echo $post->title_title . ' - '; } ?><?php echo Options::get('title'); ?></title>
 	<meta charset="UTF-8">
 	<meta name="generator" content="Habari">
+	<link rel="stylesheet/less" href="<?php echo Site::get_url('theme', '/style.less'); ?>">
 	<?php
-		//Stack::add('template_stylesheet', array('http://yui.yahooapis.com/2.8.1/build/reset-fonts-grids/reset-fonts-grids.css', 'screen,projection'), 'reset');
-		Stack::add('template_stylesheet', array('https://raw.github.com/twitter/bootstrap/master/bootstrap-1.2.0.min.css', 'screen,projection'), 'bootstrap');
-		Stack::add('template_stylesheet', array(Site::get_url('theme', '/style.css'), 'screen,projection'), 'site');
+		Stack::add('template_header_javascript', Site::get_url('theme', '/less/less-1.1.5.min.js'), 'less');
 	?>
 	<link rel="Shortcut Icon" href="<?php Site::out_url( 'theme' ); ?>/favicon.png">
 	<?php echo $theme->header(); ?>
 </head>
-<body class="<?php $theme->body_class(); ?>">
+<body class="<?php echo $theme->body_class(); ?>">
 
 <div id="wrapper">
 
-	<header id="hd">
+	<header id="header">
 		<hgroup>
 			<h1><a href="/"><?php echo Options::get('title'); ?></a></h1>
 			<h2><?php echo Options::get('tagline'); ?></h2>
@@ -25,26 +24,29 @@
 		<?php echo $theme->area('nav'); ?>
 		</nav>
 	</header>
-	<div id="bd">
-		<?php Session::messages_out(); ?>
 
-		<?php if(isset($posts) && count($posts) > 1): ?>
-			<?php foreach($posts as $post) echo $theme->content($post); ?>
-			<div class="pagenav">
-				<?php echo $theme->prev_page_link( _t( '&laquo;&nbsp;Newer&nbsp;Posts' ) ); ?>
-				<?php echo $theme->page_selector( null, array( 'leftSide' => 2, 'rightSide' => 2, 'hideIfSinglePage' => true ) ); ?>
-				<?php echo $theme->next_page_link( _t( 'Older&nbsp;Posts&nbsp;&raquo;' ) ); ?>
-			</div>
-		<?php else: ?>
-			<?php echo $theme->content($post); ?>
-		<?php endif; ?>
+	<div id="content">
+		<div id="posts">
+			<?php Session::messages_out(); ?>
+
+			<?php if(isset($posts) && count($posts) > 1): ?>
+				<?php foreach($posts as $post) echo $theme->content($post); ?>
+				<div class="pagenav">
+					<?php echo $theme->prev_page_link( _t( '&laquo;&nbsp;Newer&nbsp;Posts' ) ); ?>
+					<?php echo $theme->page_selector( null, array( 'leftSide' => 2, 'rightSide' => 2, 'hideIfSinglePage' => true ) ); ?>
+					<?php echo $theme->next_page_link( _t( 'Older&nbsp;Posts&nbsp;&raquo;' ) ); ?>
+				</div>
+			<?php else: ?>
+				<?php echo $theme->content($post); ?>
+			<?php endif; ?>
+		</div>
+
+		<aside id="sidebar">
+			<?php echo $theme->area('sidebar'); ?>
+		</aside>
 	</div>
 
-	<aside id="sidebar">
-		<?php $theme->area('sidebar'); ?>
-	</aside>
-
-  <footer id="ft"><?php $theme->area('footer'); ?></footer>
+  <footer id="footer"><?php echo $theme->area('footer'); ?></footer>
 
 </div>
 </body>
